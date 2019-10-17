1fb2a31bd7a1bc7e305c7ac5894b7162c10f9e93
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-17 00:16:48 +0000

    Node Postgres - Querying the students and teachers databases

---
+++ bootcamp_app/students.js
@@ -0,0 +1,25 @@
+// Connect to the bootcampx database 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// Query: Select some data about students
+pool.query(`
+SELECT students.id, students.name, students.cohort_id, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  })
+  // console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack));
